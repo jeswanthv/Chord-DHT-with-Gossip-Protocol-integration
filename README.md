25ce19d10dacbf9401b5dda6297ffa049317329c
surya-kangeyan <suryakangeyan4701@gmail.com>
2024-04-22 11:14:39 -0700

    added client and server py files, added data_struct.proto config file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Distributed Computing final project 
+
+## Basic File sharing
+### Proto File
+    -Describes the data structures and services in a structured format that can be automatically parsed and used to generate code.
+## How to run
+        1. ```python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. data_struct.      proto````  - to compile proto file into py code.
+        2. Replace file paths in both server.py and client.py.
+        3. Start server and run the client.
+
+        
+        
+### Team
+    1. Suryakangeyan
+    2. Sahil salim
+    3. Aaksah 
---
+++ server/server.py
@@ -0,0 +1,32 @@
+from concurrent import futures
+import grpc
+import os
+import prot_buffers.data_struct_pb2 as data_struct_pb2
+import prot_buffers.data_struct_pb2_grpc as data_struct_pb2_grpc
+
+class FileService(data_struct_pb2_grpc.FileServiceServicer):
+    
+    def RequestFile(self, request, context):
+        file_path = os.path.join('server path', request.filename)
+        try:
+            with open(file_path, 'rb') as f:
+                while True:
+                    piece = f.read(1024)  # Read file in chunks of 1024 bytes
+                    if not piece:
+                        break
+                    yield data_struct_pb2.FileResponse(data=piece)
+        except FileNotFoundError:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details('File not found')
+            return
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    data_struct_pb2_grpc.add_FileServiceServicer_to_server(FileService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server listening on port 50051")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
